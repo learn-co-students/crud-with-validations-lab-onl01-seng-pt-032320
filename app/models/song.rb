933b29a37b0838ff7883b3387e2e3604c6bbf15a
Vlad <VladDaImpaler@gmail.com>
2020-08-16 14:43:23 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,9 @@
 class Song < ApplicationRecord
     validates :title, :artist_name, presence: true
-    validates :title, uniqueness: { scope: [:year, :artist_name] }
+    validates :title, uniqueness: { scope: [:release_year, :artist_name] }
     validates :released, inclusion: [true, false]
-    validates :release_year, presence: true, if: :released
-    validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.new.year }
+    with_options if: :released? do |o|
+        o.validates :release_year, presence: true
+        o.validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.new.year }
+    end
 end
