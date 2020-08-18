e7e6b761f38b96cacf9a867c57c8f3a27ad25ec8
swade781977 <swade781977@yahoo.com>
2020-08-17 23:05:19 -0400

    Done.

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: 'The Boxer', released: true, released_year: '1970', artist: 'Simon & Garfunkel', genre: 'folk')
+Song.create(title: 'Pink Lemonade', released: true, released_year: '2016', artist: 'Watsky', genre: 'Rap')
